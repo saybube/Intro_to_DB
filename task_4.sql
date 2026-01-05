697587dc73d22ba6021245e281102455a928ac54
saybube <ebubenwamaka@gmail.com>
2026-01-05 01:35:01 +0100

    update

--- task_4.sql
+++ task_4.sql
@@ -1,21 +1,26 @@
 USE alx_book_store;
 
--- Show full description of the Books table
-SHOW COLUMNS FROM Books;
-
+-- Select full description of the Books table from INFORMATION_SCHEMA
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
 /* 
-mysql -u root -p alx_book_store < task_4.sql
-```
-
-**Expected Output:**
-```
-+------------------+--------------+------+-----+---------+----------------+
-| Field            | Type         | Null | Key | Default | Extra          |
-+------------------+--------------+------+-----+---------+----------------+
-| book_id          | int          | NO   | PRI | NULL    | auto_increment |
-| title            | varchar(130) | NO   |     | NULL    |                |
-| author_id        | int          | YES  | MUL | NULL    |                |
-| price            | double       | NO   |     | NULL    |                |
-| publication_date | date         | YES  |     | NULL    |                |
-+------------------+--------------+------+-----+---------+----------------+
++------------------+--------------+-------------+------------+----------------+----------------+
+| COLUMN_NAME      | COLUMN_TYPE  | IS_NULLABLE | COLUMN_KEY | COLUMN_DEFAULT | EXTRA          |
++------------------+--------------+-------------+------------+----------------+----------------+
+| book_id          | int          | NO          | PRI        | NULL           | auto_increment |
+| title            | varchar(130) | NO          |            | NULL           |                |
+| author_id        | int          | YES         | MUL        | NULL           |                |
+| price            | double       | NO          |            | NULL           |                |
+| publication_date | date         | YES         |            | NULL           |                |
++------------------+--------------+-------------+------------+----------------+----------------+
 */
