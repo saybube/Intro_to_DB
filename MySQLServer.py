46bd61658e6c409d0f91fd2403812b67e59acb68
saybube <ebubenwamaka@gmail.com>
2026-01-05 01:20:56 +0100

    changed except error

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,7 +9,7 @@ def create_database():
         connection = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='your_password'  # Replace with your MySQL password
+            password='your_password'  # Replace with MySQL password
         )
         
         if connection.is_connected():
@@ -22,7 +22,7 @@ def create_database():
             
             cursor.close()
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
         
     finally:
