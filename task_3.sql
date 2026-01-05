f04fe5469a19b328e4311621db2060a7b49bb0a1
saybube <ebubenwamaka@gmail.com>
2026-01-05 01:27:30 +0100

    list all tables created

---
+++ task_3.sql
@@ -0,0 +1,21 @@
+SHOW TABLES;
+
+-- mysql -u root -p alx_book_store < task_3.sql
+
+/* mysql -u root -p -D alx_book_store < task_3.sql
+```
+
+**Expected Output:**
+```
++---------------------------+
+| Tables_in_alx_book_store  |
++---------------------------+
+| Authors                   |
+| Books                     |
+| Customers                 |
+| Order_Details             |
+| Orders                    |
++---------------------------+ 
+
+Since the database name will be passed as an argument to the mysql command, you run it like this:
+*/
